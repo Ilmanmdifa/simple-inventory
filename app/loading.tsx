d492ba2ee49f6757fc4f486d78d2615b99307b7a
Ilmanmdifa <ilmanmdifa63@gmail.com>
2026-01-21 16:27:57 +0700

    add settings page and loading

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,6 +1,14 @@
-export default function Loading() {
+import SideBar from "./components/sidebar";
 
+export default function Loading() {
   // Stack uses React Suspense, which will render this page while user data is being fetched.
   // See: https://nextjs.org/docs/app/api-reference/file-conventions/loading
-  return <></>;
+  return (
+    <>
+      <SideBar currentPath="/" />
+      <div className="flex justify-center items-center h-screen">
+        <div className="animate-spin rounded-full h-32 w-32 border-b-4 border-purple-500"></div>
+      </div>
+    </>
+  );
 }
